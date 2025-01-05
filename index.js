3314dabe5b3f948e16c94522b32ba29a2571fa38
kirankumar <48343808+kirankumar485@users.noreply.github.com>
2025-01-05 12:27:41 +0530

    updated env

--- index.js
+++ index.js
@@ -2,23 +2,27 @@ const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
 const path = require("path");
+require('dotenv').config();
 
 const authRoutes = require("./routes/authRoutes");
 const userRoutes = require("./routes/userRoutes");
 
 const app = express();
 const PORT = 5000;
-
 // Middleware
 app.use(cors());
 app.use(express.json());
 app.use("/uploads", express.static(path.join(__dirname, "uploads")));
 
 // Database connection
-mongoose.connect("mongodb://localhost:27017/mernapp", {
+const uri = process.env.MONGODB_URI;
+
+mongoose.connect(uri, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
-});
+})
+    .then(() => console.log("Database connected successfully"))
+    .catch((err) => console.error("Database connection error:", err));
 
 app.use("/api/auth", authRoutes);
 app.use("/api", userRoutes);
